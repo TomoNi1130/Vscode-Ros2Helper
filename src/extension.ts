659c73366e5fc4edd72c319cc95a322d215f7eca
Tomoni11 <171126805+TomoNi1130@users.noreply.github.com>
2025-12-08 22:03:54 +0900

    inital commit

---
+++ src/extension.ts
@@ -0,0 +1,28 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {//主要なエントリポイント
+	//すべての拡張機能のセットアップ、コマンド登録、イベントリスナーの購読はここでの実行。
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "ros2helper" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('ros2helper.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from ros2-helper!');
+	});
+
+	context.subscriptions.push(disposable);
+	//↑拡張機能が無効化されたときに自動で解除するために管理リストへ登録
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
